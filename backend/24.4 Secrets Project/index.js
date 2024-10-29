3facc8a396eecb6cdd63f819be591a0cc4aabbfe
Marvellous Oduwole <codedmarve@gmail.com>
2024-10-29 18:26:55 +0100

    simple password check site

--- backend/24.4 Secrets Project/index.js
+++ backend/24.4 Secrets Project/index.js
@@ -1,3 +1,42 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const port = 3000;
+var match = false;
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+function passwordCheck(req, res , next) {
+
+	if (req.body.password === "ILoveProgramming")
+		match = true;
+	next();
+}
+
+app.use(passwordCheck);
+
+app.post("/check", (req, res) => {
+
+	console.log(req.body);
+	if (match)
+		res.sendFile(__dirname + "/public/secret.html");
+	match = false;
+});
+
+app.get("/", (req, res) => {
+
+	res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
